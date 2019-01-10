a0a2056d14cd19bf30fe46a32f890618d45ccfe0
Emcoe <45089242+Emcoe@users.noreply.github.com>
2019-01-10 00:09:02 -0800

    Adding first try at text input to eliminate need for users to edit prefs.txt for first run

--- peri-auto.py
+++ peri-auto.py
@@ -1,6 +1,7 @@
-import wget
+
 import os
 import subprocess
+import wget
 
 try:
     os.remove("peri-auto.wget")
@@ -10,74 +11,98 @@ except FileNotFoundError:
 try:
     with open('prefs.txt') as f:
         targetURL = f.readline().strip()
-    wget.download(targetURL, "peri-auto.wget")
+    catchupflag = False
+except FileNotFoundError:
+    catchupflag = True
+    newtarget = input("Welcome to peri-auto! Just for the first time, what is "
+                      "the name username of the Periscoper you're following? "
+                      "For 'https://www.periscope.tv/ABC/' you would type "
+                      "'ABC' (without quotemarks): ")
+# How to handle incorrect username input?
+    targetURL = "https://www.periscope.tv/" + newtarget + "/"
 
-    with open('peri-auto.wget', 'r') as veryrawsource:
-        rawsource = veryrawsource.read().replace('\n', '')
-    startpos = 33 + rawsource.find("{&quot;broadcastIds&quot;:[&quot;")
-    segment = rawsource[startpos:]
-    endpos = segment.find("]") - 6
-    segment = segment[:endpos]
-    results = segment.split('&quot;,&quot;')
+wget.download(targetURL,
+              "peri-auto.wget")
 
-    try:
-        os.remove("peri-auto.wget")
-    except FileNotFoundError:
-        pass
+with open('peri-auto.wget',
+          'r') as veryrawsource:
+    rawsource = veryrawsource.read().replace('\n', '')
+startpos = 33 + rawsource.find("{&quot;broadcastIds&quot;:[&quot;")
+segment = rawsource[startpos:]
+endpos = segment.find("]") - 6
+segment = segment[:endpos]
+results = segment.split('&quot;,&quot;')
+resultsnum = len(results)
+print('')
+print('Broadcasts found on', targetURL, ":", resultsnum)
 
-    print('')
-    print('Broadcasts found on', targetURL, ":", len(results))
+try:
+    os.remove("peri-auto.wget")
+except FileNotFoundError:
+    pass
 
-    golist = []
-    entry = ""
 
-    with open('prefs.txt', 'r') as veryrawsource:
-        rawsource = veryrawsource.read().replace('\n', '')
+golist = []
+entry = ""
+
+if catchupflag is True:
+    catchup = input("Peri-auto will get all new " + newtarget + " broadcasts "
+                    "going forward, but since we're starting fresh, you can "
+                    "choose to grab up to " + str(resultsnum) + " of their "
+                    "previous broadcasts. How far back do you want to go? "
+                    "Enter a number from 0-" + str(resultsnum) + ": ")
+    while (int(catchup) > resultsnum) or (int(catchup) < 0):
+        catchup = input("Please enter a number from 0-" + str(resultsnum)
+                        + ": ")
+    with open('prefs.txt', 'a') as f:
+        f.write("https://www.periscope.tv/" + newtarget + "/\n-----Above this "
+                "line is the Periscoper's URL, and below are any broadcasts "
+                "you've already seen-----\n")
+
+with open('prefs.txt', 'r') as veryrawsource:
+    rawsource = veryrawsource.read().replace('\n', '')
+
+if catchupflag is False:
     for entry in results:
         if rawsource.find(str(entry)) is -1:
             cleanup = "https://www.pscp.tv/w/" + entry
             golist.append(cleanup)
     print('Broadcasts not already in prefs.txt:', len(golist))
+else:
+    for entry in results:
+        if rawsource.find(str(entry)) is -1:
+            cleanup = "https://www.pscp.tv/w/" + entry
+            golist.append(cleanup)
+        golist = golist[:int(catchup)]
 
-    if len(golist) > 0:
-        open('todolist.txt', 'w').close()
-        for entry in golist:
-            with open('todolist.txt', 'a') as final:
-                final.write('\n'+entry)
-        print('List written')
+if len(golist) > 0:
+    open('todolist.txt', 'w').close()
+    for entry in golist:
+        with open("todolist.txt", 'a') as final:
+            final.write('\n'+entry)
+    print('List written')
 
-        subprocess.call(["youtube-dl", "-o",
-                         "%(upload_date)s-%(title)s-%(id)s.%(ext)s", "-x",
-                         "--audio-format", "m4a", "--audio-quality", "64K",
-                         "--batch-file", "todolist.txt"])
+    subprocess.call(["youtube-dl", "-o",
+                     "%(upload_date)s-%(title)s-%(id)s.%(ext)s",
+                     "-x", "--audio-format", "m4a", "--audio-quality",
+                     "64K", "--batch-file", "todolist.txt"])
 
+    with open('prefs.txt', 'a') as final:
+        final.write('\n')
+    for entry in golist:
         with open('prefs.txt', 'a') as final:
-            final.write('\n')
-        for entry in golist:
-            with open('prefs.txt', 'a') as final:
-                final.write('\n'+entry)
-        with open('backuplistofseenURLs.txt', 'a') as final:
-            final.write('\n')
-        for entry in golist:
-            with open('backuplistofseenURLs.txt', 'a') as final:
-                final.write('\n'+entry)
-        try:
-            os.remove("peri-auto.wget")
-        except FileNotFoundError:
-            pass
-        try:
-            os.remove("todolist.txt")
-        except FileNotFoundError:
-            pass
-        print('Success!')
-
-
-except FileNotFoundError:
-    print("prefs.txt created, change the first line to the URL of the "
-          "Periscoper you're trying to download from. Below the first line, "
-          "paste in URLs of any broadcasts you've already seen, and don't "
-          "want to download!")
-    with open('prefs.txt', 'a') as f:
-        f.write("https://www.periscope.tv/ABC/\n-----Above this line is the "
-                "Periscoper's URL you want to download, and below are any "
-                "broadcasts you've already seen-----\n")
+            final.write('\n'+entry)
+    with open("backuplistofseenURLs.txt", 'a') as final:
+        final.write('\n')
+    for entry in golist:
+        with open("backuplistofseenURLs.txt", 'a') as final:
+            final.write('\n'+entry)
+    try:
+        os.remove("peri-auto.wget")
+    except FileNotFoundError:
+        pass
+    try:
+        os.remove("todolist.txt")
+    except FileNotFoundError:
+        pass
+    print('Success!')
