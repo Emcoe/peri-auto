b4c321e531e3a413351a641abd74fc906cc32064
Emcoe <45089242+Emcoe@users.noreply.github.com>
2019-02-10 17:32:56 -0800

    Overhauled first time startup, added automatic retrying if youtube-dl errors, and some restructuring

--- peri-auto.py
+++ peri-auto.py
@@ -3,6 +3,37 @@ import os
 import subprocess
 import wget
 
+audiopath = ""
+print('Peri-auto v1.1')
+
+
+def print_lists():
+    with open('prefs.txt',
+              'a') as final:
+        final.write('\n')
+    for entry in golist:
+        with open('prefs.txt',
+                  'a') as final:
+            final.write('\n'+entry)
+    with open("backuplistofseenURLs.txt", 'a') as final:
+        final.write('\n')
+    for entry in golist:
+        with open(
+                  "backuplistofseenURLs.txt", 'a') as final:
+            final.write('\n'+entry)
+    try:
+        os.remove(
+                  "peri-auto.wget")
+    except FileNotFoundError:
+        pass
+    try:
+        os.remove(
+                  "todolist.txt")
+    except FileNotFoundError:
+        pass
+    print('Success!')
+
+
 try:
     os.remove("peri-auto.wget")
 except FileNotFoundError:
@@ -11,6 +42,9 @@ except FileNotFoundError:
 try:
     with open('prefs.txt') as f:
         targetURL = f.readline().strip()
+        line2 = f.readline().strip()
+        if line2[0:4] != "-----":
+            audiopath = line2
     catchupflag = False
 except FileNotFoundError:
     catchupflag = True
@@ -54,54 +88,48 @@ if catchupflag is True:
     while (int(catchup) > resultsnum) or (int(catchup) < 0):
         catchup = input("Please enter a number from 0-" + str(resultsnum)
                         + ": ")
-    with open('prefs.txt', 'a') as f:
+    with open('prefs.txt',
+              'a') as f:
         f.write("https://www.periscope.tv/" + newtarget + "/\n-----Above this "
                 "line is the Periscoper's URL, and below are any broadcasts "
                 "you've already seen-----\n")
 
-with open('prefs.txt', 'r') as veryrawsource:
-    rawsource = veryrawsource.read().replace('\n', '')
 
+with open('prefs.txt', 'r') \
+        as veryrawsource:
+    rawsource = veryrawsource.read().replace('\n', '')
 if catchupflag is False:
     for entry in results:
         if rawsource.find(str(entry)) is -1:
-            cleanup = "https://www.pscp.tv/w/" + entry
-            golist.append(cleanup)
+            golist.append("https://www.pscp.tv/w/" + entry)
     print('Broadcasts not already in prefs.txt:', len(golist))
 else:
     for entry in results:
         if rawsource.find(str(entry)) is -1:
-            cleanup = "https://www.pscp.tv/w/" + entry
-            golist.append(cleanup)
+            golist.append("https://www.pscp.tv/w/" + entry)
         golist = golist[:int(catchup)]
 
 if len(golist) > 0:
-    open('todolist.txt', 'w').close()
+    open('todolist.txt',
+         'w').close()
     for entry in golist:
-        with open("todolist.txt", 'a') as final:
+        with open(
+                  "todolist.txt", 'a') as final:
             final.write('\n'+entry)
 
-    subprocess.call(["youtube-dl", "-o",
-                     "%(upload_date)s-%(title)s-%(id)s.%(ext)s",
-                     "-x", "--audio-format", "m4a", "--audio-quality",
-                     "64K", "--batch-file", "todolist.txt"])
+    importcommand = ["youtube-dl", "-o", audiopath, "-x", "--audio-format",
+                     "m4a", "--audio-quality", "64K", "--batch-file",
+                     "todolist.txt"]
 
-    with open('prefs.txt', 'a') as final:
-        final.write('\n')
-    for entry in golist:
-        with open('prefs.txt', 'a') as final:
-            final.write('\n'+entry)
-    with open("backuplistofseenURLs.txt", 'a') as final:
-        final.write('\n')
-    for entry in golist:
-        with open("backuplistofseenURLs.txt", 'a') as final:
-            final.write('\n'+entry)
-    try:
-        os.remove("peri-auto.wget")
-    except FileNotFoundError:
-        pass
-    try:
-        os.remove("todolist.txt")
-    except FileNotFoundError:
-        pass
-    print('Success!')
+    retries = 5
+    count = 1
+    while retries >= count:
+        try:
+            subprocess.check_call(importcommand)
+            print("subprocess", count, "succeeded, printing:")
+            print_lists()
+            print("printed", count)
+            break
+        except subprocess.CalledProcessError:
+            print("subprocess", count, "failed")
+            count += 1
